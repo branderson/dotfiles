06b10308627da27984b0154e85492fe90896b64f
Brad Anderson <brad.anderson1995@gmail.com>
2021-03-28 18:51:15 -0700

    Fix tmux

--- tmux/verify_tmux_version.sh
+++ tmux/verify_tmux_version.sh
@@ -4,6 +4,11 @@ verify_tmux_version() {
     tmux_home=~/.tmux
     tmux_version="$(tmux -V | cut -c 6-)"
 
+    # This is necessary because 3.1c gives a syntax error in bc
+    # TODO Fix this it will break with the next tmux version
+    if [[ $tmux_version -eq "3.1c"]] ; then
+        tmux source-file "$tmux_home/tmux_3.1.conf"
+        exit
     if [[ $(echo "$tmux_version >= 2.5" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_2.5_up.conf"
         exit
