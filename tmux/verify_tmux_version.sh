c144e7b6eeccbe06e045a2d14f7470e34391b36a
Brad Anderson <bradleya@yelp.com>
2021-06-28 12:56:43 -0700

    Adding changes from work devbox

--- tmux/verify_tmux_version.sh
+++ tmux/verify_tmux_version.sh
@@ -7,19 +7,19 @@ verify_tmux_version() {
     # This is necessary because 3.1c gives a syntax error in bc
     # TODO Fix this it will break with the next tmux version
     if [[ $tmux_version -eq "3.1c"]] ; then
-        tmux source-file "$tmux_home/tmux_3.1.conf"
+        tmux source-file $tmux_home/tmux_3.1c.conf
         exit
     if [[ $(echo "$tmux_version >= 2.5" | bc) -eq 1 ]] ; then
-        tmux source-file "$tmux_home/tmux_2.5_up.conf"
+        tmux source-file $tmux_home/tmux_3.1c.conf
         exit
     elif [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
-        tmux source-file "$tmux_home/tmux_2.1_to_2.5.conf"
+        tmux source-file $tmux_home/tmux_2.1_to_2.5.conf
         exit
     elif [[ $(echo "$tmux_version >= 1.9" | bc) -eq 1 ]] ; then
-        tmux source-file "$tmux_home/tmux_1.9_to_2.1.conf"
+        tmux source-file $tmux_home/tmux_1.9_to_2.1.conf
         exit
     else
-        tmux source-file "$tmux_home/tmux_1.9_down.conf"
+        tmux source-file $tmux_home/tmux_1.9_down.conf
         exit
     fi
 }
