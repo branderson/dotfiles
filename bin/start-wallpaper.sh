2a8051c68bc8208d643dbe0cdcda0aaec6912fbe
Brad Anderson <brad@branderson.io>
2025-08-08 15:20:44 -0700

    Update wallpaper script to detect display layout and set wallpapers appropriately

--- bin/start-wallpaper.sh
+++ bin/start-wallpaper.sh
@@ -1,10 +1,55 @@
 #!/bin/bash
+source ~/dotfiles/functions.sh
 
-# Wide
-# feh --bg-fill "$(shuf -en1 ~/wallpapers/*)"
+wallpapers_path="$HOME/wallpapers"
+wallpapers_vertical_path="$HOME/wallpapers-vertical"
 
-# Wide - Wide - Vertical
-# feh --bg-fill "$(shuf -en1 ~/wallpapers/*)" "$(shuf -en1 ~/wallpapers/*)"
+if [ ! -d "$wallpapers_path" ]; then
+    echo "ERROR: Wallpaper directory not found!"
+    echo "Please create wallpapers directory in $wallpapers_path"
+    exit 1
+fi
 
-# Wide - Wide - Vertical
-feh --bg-fill "$(shuf -en1 ~/wallpapers/*)" "$(shuf -en1 ~/wallpapers/*)" "$(shuf -en1 ~/wallpapers-vertical/*)"
+if [ $(program_installed xrandr) == 1 ]; then
+    display_count="$(xrandr | grep ' connected ' | wc -l)"
+    # Get the selected resolution for each display
+    display_resolutions="$(xrandr | grep '*' | awk '{ print $1}')"
+    # Get the rotation for each display, need verbose to return rotation if normal
+    # and remove 'primary' for consistent column placement
+    display_rotations="$(xrandr --query --verbose | grep ' connected ' | sed 's/primary //' | awk '{print $5}')"
+    # Build feh command
+    feh_args=("--bg-fill")
+    for i in $(seq 1 "$display_count"); do
+        resolution="$(echo "$display_resolutions" | sed "${i}q;d")"
+        rotation="$(echo "$display_rotations" | sed "${i}q;d")"
+        path="$wallpapers_path"
+        resolution_segment=""
+        if [ "$rotation" != "normal" ]; then
+            if [ ! -d "$wallpapers_vertical_path" ]; then
+                echo "ERROR: Vertical display detected, but vertical wallpapers directory not found!"
+                echo "Please create vertical wallpapers directory in $wallpapers_vertical_path"
+                exit 1
+            fi
+            path="$wallpapers_vertical_path"
+        fi
+        # If we have a resolution-specific path for the display, use it
+        if [ -d "$path/1080p" ]; then
+            resolution_segment="/1080p"
+        fi
+        if [ -d "$path/4k" ]; then
+            if [ "$resolution" = "3840x2160" ] || [ "$resolution" = "2160x3840" ]; then
+                resolution_segment="/4k"
+            fi
+        fi
+        display_wallpaper="$(shuf -en1 "$path$resolution_segment"/*)"
+        feh_args+=("$display_wallpaper")
+    done
+    feh "${feh_args[@]}"
+else
+    # Default to ~/wallpapers/1080p or ~/wallpapers for first display if no xrandr
+    if [ -d "$path/1080p" ]; then
+        feh --bg-fill "$(shuf -en1 $wallpapers_path/1080p/*)"
+    else
+        feh --bg-fill "$(shuf -en1 $wallpapers_path/*)"
+    fi
+fi
