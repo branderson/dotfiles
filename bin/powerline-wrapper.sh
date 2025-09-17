ff49aba967f4a859e182f6944f147c69be6759c3
Brad Anderson <bradleya@yelp.com>
2025-09-16 17:12:05 -0700

    Make powerline support python virtualenvs without breaking prompt

---
+++ bin/powerline-wrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Wrapper script around powerline to force a PYTHONPATH without setting it globally
+
+source "$HOME/.bash_functions"
+set_powerline_bash_sources
+found=0
+# Find site_packages with powerline installed
+for index in ${!POWERLINE_BASH_SOURCES[@]}; do
+    file="${POWERLINE_BASH_SOURCES[index]}"
+    if [ -a "$file" ]; then
+        # Get the corresponding site-packages root
+        site_packages="$file"
+        for i in {1..4}; do
+            site_packages=$(dirname "$site_packages")
+        done
+        found=1
+        break
+    fi
+done
+
+if [ $found == 1 ]; then
+    PYTHONPATH="$site_packages" powerline "$@"
+else
+    echo "Powerline site_packages not found"
+    exit 1
+fi
